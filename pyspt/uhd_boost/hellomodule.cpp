e0c888c5ea9ee9f329c1adc0b73fc139dd379459
mgu <Martin.Guski@gmail.com>
2016-12-04 12:44:48 -0900

    included uhd library

--- pyspt/uhd_boost/hellomodule.cpp
+++ pyspt/uhd_boost/hellomodule.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <Python.h>
-#include <uhd/types/device_addr.hpp>
-#include <uhd/device.hpp>
+#include <uhd.h>
+
+
+#include <uhd/types/tune_request.hpp>
+#include <uhd/utils/thread_priority.hpp>
+#include <uhd/utils/safe_main.hpp>
+#include <uhd/usrp/multi_usrp.hpp>
+#include <uhd/transport/udp_simple.hpp>
+#include <uhd/exception.hpp>
+#include <uhd/utils/msg.hpp>
+
+
 
 using namespace std;
 
@@ -9,12 +19,57 @@ void say_hello(const char* name) {
     cout << "Hello " <<  name << "!\n";
 }
 
-void find_device() {
+void find_device_old() {
         cout << "laeuft\n";
+       uhd::time_spec_t get_data_t0;
+
+
   	uhd::device_addr_t hint; //an empty hint discovers all devices
-//	uhd::device_addrs_t dev_addrs = uhd::device::find(hint);
-//        cout << dev_addrs[0].to_string() ;
+	uhd::device_addrs_t dev_addrs = uhd::device::find(hint);
+
+        cout << dev_addrs[0].to_string() ;
 }
+
+PyObject* find_device() {
+        cout << "Searching for devices ...\n";
+        uhd::time_spec_t get_data_t0;
+
+
+  	uhd::device_addr_t hint; //an empty hint discovers all devices
+	uhd::device_addrs_t dev_addrs = uhd::device::find(hint);
+        for (size_t i = 0; i < dev_addrs.size(); i++) {
+            cout << dev_addrs[i].to_pp_string() << endl;
+          }
+//        cout << dev_addrs[0].to_string() ;
+        PyObject *pArgs;
+     //   PyTuple_SetItem(pArgs, 0,PyUnicode_FromString(dev_addrs[0].to_string()) );
+
+        PyObject *tuple, *list;
+
+        tuple = Py_BuildValue("(iis)", 1, 2, "three");
+        list = Py_BuildValue("[iis]", 1, 2, "three");
+     //   Py_XDECREF(list);
+        return list;
+
+}
+
+
+/*
+// create streamer
+uhd::stream_args_t stream_args("fc32", "sc16");
+// 2. Set the channel list, we want 3 streamers coming from channels
+//    0, 1 and 2, in that order:
+stream_args.channels = boost::assign::list_of(0)(1)(2);
+// 3. Set optional args:
+stream_args.args["spp"] = "200"; // 200 samples per packet
+// Now use these args to create an rx streamer:
+// (We assume that usrp is a valid uhd::usrp::multi_usrp)
+uhd::rx_streamer::sptr rx_stream = usrp->get_rx_stream(stream_args);
+
+*/
+
+
+
 
 #include <boost/python/module.hpp>
 #include <boost/python/def.hpp>
--- pyspt/uhd_boost/setup.py
+++ pyspt/uhd_boost/setup.py
@@ -2,12 +2,17 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-reposPath = "/home/mguski/repos/"
-uhdPath = reposPath + "uhd/host/include/"
+#reposPath = "/home/mguski/repos/"
+#reposPath = "../../../../"
+#uhdPath = reposPath + "uhd/host/include/"
+
+uhdPath = '/home/radar/uhd/host/'
 
 setup(name="PackageName",
     ext_modules=[
         Extension("hello", ["hellomodule.cpp"],
-        include_dirs = [uhdPath,  uhdPath + 'uhd', uhdPath+'uhd/types', uhdPath+'uhd/usrp'],
-        libraries = ["boost_python", reposPath + "uhd/host/lib", "/usr/lib/x86_64-linux-gnu/libpython2.7.a"])
-    ])
+        include_dirs = ["/home/radar/uhd/host/include", "/home/radar/uhd/host/include/uhd/types", "/home/radar/uhd/host/include/uhd/usrp"  ],
+        libraries = ["boost_python", "uhd"],
+        library_dirs = [  "/home/radar/repos/uhd/host/build/lib","/home/radar/uhd/host/build/lib","/home/radar/uhd/host/lib", "/home/radar/uhd/host/build/lib/CMakeFiles/"  ])
+ ])
+
